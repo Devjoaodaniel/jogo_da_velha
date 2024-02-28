f8f56d10180c5139d0ae3a0b79663d4c81c9077c
João Daniel da Silva <159394359+Devjoaodaniel@users.noreply.github.com>
2024-02-28 09:56:02 -0300

    jogo_da_velha_0.2🚀

---
+++ READm.md
@@ -0,0 +1,23 @@
+# Jogo da Velha - Desafie sua Estratégia!
+
+Bem-vindo ao Jogo da Velha! Este é um jogo clássico, mas cativante, projetado para testar suas habilidades estratégicas. Desafie-se ou convide um amigo para uma partida e veja quem consegue alinhar três símbolos na horizontal, vertical ou diagonal primeiro!
+
+## Como Jogar:
+1. Abra o arquivo `index.html` em seu navegador preferido.
+2. Clique em qualquer célula vazia do tabuleiro para marcar com "X" ou "O".
+3. O jogo alternará automaticamente entre os jogadores após cada movimento.
+4. O primeiro jogador a conseguir alinhar três símbolos na horizontal, vertical ou diagonal será o vencedor!
+5. Se todas as células forem preenchidas sem um vencedor, o jogo terminará em empate.
+
+## Recursos:
+- **Design Simples:** Interface minimalista para uma experiência de jogo envolvente.
+- **Feedback Visual:** As células do tabuleiro mudam de cor ao passar o mouse, proporcionando uma interação clara.
+- **Notificação de Vitória:** Alerta instantâneo quando um jogador alcança a vitória.
+- **Reinício Rápido:** Botão de reinício para começar uma nova partida a qualquer momento.
+
+## Tecnologias Utilizadas:
+- **HTML:** Estrutura básica da página web.
+- **CSS:** Estilização para uma apresentação visual agradável e responsiva.
+- **JavaScript:** Lógica de jogo dinâmica e manipulação de eventos.
+
+Desfrute de momentos divertidos e desafiadores com este clássico jogo da velha! Que vença o melhor estrategista! 🎮✨
--- java.js
+++ java.js
@@ -1,16 +1,78 @@
-let jogador = "x"
-function jogar(celula){
-    if(celula.innerHTML ==""){
-        celula.innerHTML = jogador
+let jogador
+let tabuleiro
 
-    if(jogador == "x"){
-            jogador = "o"
-        }else{
-            jogador = "x"
-        }
+function marcar(indice) {
+    tabuleiro[indice] = jogador
+    trocarJogador()
+    renderizar()
+    verificarVitoria()
+}
+
+function trocarJogador() {
+    jogador = jogador == "X" ? "O" : "X"
+}
+
+function renderizar() {
+    // jogador na tela
+    const b = document.getElementById("jogador")
+    b.innerText = jogador
+
+    // tabuleiro na tela
+    const tds = document.querySelectorAll("#tabuleiro td")
+    for(let i = 0; i < tabuleiro.length; i++) {
+        tds[i].innerText = tabuleiro[i]
     }
 }
-function reiniciar(){
-    window.location.reload()
+
+function verificarVitoria() {
+    //horizontal
+    if (tabuleiro[0] && tabuleiro[0] == tabuleiro[1] && tabuleiro[1] == tabuleiro[2]) {
+        alert("HOUVE VITORIA!")
+    }
+    if (tabuleiro[3] && tabuleiro[3] == tabuleiro[4] && tabuleiro[4] == tabuleiro[5]) {
+        alert("HOUVE VITORIA!")
+    }
+    if (tabuleiro[6] && tabuleiro[6] == tabuleiro[7] && tabuleiro[7] == tabuleiro[8]) {
+        alert("HOUVE VITORIA!")
+    }
+    if (tabuleiro[7] && tabuleiro[7] == tabuleiro[8] && tabuleiro[8] == tabuleiro[9]) {
+        alert("HOUVE VITORIA!")
+    }
+    //vertical
+    if (tabuleiro[0] && tabuleiro[0] == tabuleiro[3] && tabuleiro[3] == tabuleiro[6]) {
+        alert("HOUVE VITORIA!")
+    }
+    if (tabuleiro[1] && tabuleiro[1] == tabuleiro[4] && tabuleiro[4] == tabuleiro[7]) {
+        alert("HOUVE VITORIA!")
+    }
+
+    if (tabuleiro[2] && tabuleiro[2] == tabuleiro[5] && tabuleiro[5] == tabuleiro[8]) {
+        alert("HOUVE VITORIA!")
+    }
+
+    // diagonal
+    if (tabuleiro[0] && tabuleiro[0] == tabuleiro[4] && tabuleiro[4] == tabuleiro[8]) {
+        alert("HOUVE VITORIA!")
+    }
+
+    if (tabuleiro[2] && tabuleiro[2] == tabuleiro[4] && tabuleiro[4] == tabuleiro[6]) {
+        alert("HOUVE VITORIA!")
+    }
+
+    // verificar se deu velha
+    let deuVelha = true
+    for(let i = 0; i < tabuleiro.length; i++) {
+        if (!tabuleiro[i]) {
+            deuVelha = false
+        }
+    }
+    if (deuVelha) alert("DEU VELHA!")
 }
 
+function iniciar() {
+    jogador = "X"
+    tabuleiro = ["", "", "", "", "", "", "", "", ""]
+    renderizar()
+}
+
+iniciar()
